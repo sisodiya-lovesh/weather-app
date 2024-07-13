f506a1b25a86ba32889e68b5a9a8e66ae38455a2
sisodiya-lovesh <lk1234good@gmail.com>
2024-07-13 15:25:54 +0530

    Weather App

---
+++ src/WeatherSearch.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './WeatherSearch.css';
+
+const WeatherSearch = () => {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const apiKey = '7ad2425e5186449e865173835242803';
+
+  const fetchWeatherData = async () => {
+    if (!city) {
+      setError('Please enter a city name');
+      return;
+    }
+
+    setLoading(true);
+    setError('');
+    try {
+      const response = await axios.get(
+        `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
+      );
+      setWeatherData(response.data);
+    } catch (error) {
+     alert('Failed to fetch weather data');
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div className="weather-app">
+      <h1>Weather Application</h1>
+      <input
+        type="text"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        placeholder="Enter city name"
+      />
+      <button onClick={fetchWeatherData}>Search</button>
+      {loading && <p className="loading">Loading data…</p>}
+      {error && <p className="error">{error}</p>}
+      {weatherData && (
+        <div className="weather-cards">
+          <div className="weather-card">
+            <p><strong>Temperature</strong><br />{weatherData.current.temp_c}°C</p>
+          </div>
+          <div className="weather-card">
+            <p><strong>Humidity</strong><br />{weatherData.current.humidity}%</p>
+          </div>
+          <div className="weather-card">
+            <p><strong>Condition</strong><br />{weatherData.current.condition.text}</p>
+          </div>
+          <div className="weather-card">
+            <p><strong>Wind Speed</strong><br />{weatherData.current.wind_kph} kph</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default WeatherSearch;
